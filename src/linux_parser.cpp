1fdbbcab0e98b253965b93ba04b4e1be7ee5195d
gdangelo <dangelo.gregory@gmail.com>
2019-10-21 10:26:35 +0000

    feat: parse the system uptime from /proc/uptime file

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -70,8 +70,18 @@ vector<int> LinuxParser::Pids() {
 // TODO: Read and return the system memory utilization
 float LinuxParser::MemoryUtilization() { return 0.0; }
 
-// TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+// Read and return the system uptime
+long LinuxParser::UpTime() { 
+  string uptime, line;
+  std::ifstream stream(kProcDirectory + kUptimeFilename);
+  if (stream.is_open()) {
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+    linestream >> uptime;
+    return std::stol(uptime);
+  }
+  return 0; 
+}
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
